df43a00326ab48a56d6e2552a8d710b19495f283
Ravi Shah <amisankhesara@gmail.com>
2019-02-20 23:44:15 -0800

    added Hello.java

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Amee!!");
+
+        int myfirstnumber = (10+5)+(2*10);
+        int mysecondnumber = 12;
+        int mythirdnumber = myfirstnumber*2;
+
+        int mytotal = myfirstnumber + mysecondnumber + mythirdnumber;
+
+        int x= 1000-mytotal;
+
+        System.out.println(x);
+    }
+
+}
