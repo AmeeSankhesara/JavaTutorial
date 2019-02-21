d436737344626c23377180618c16b22020c20d91
Ravi Shah <amisankhesara@gmail.com>
2019-02-20 23:49:06 -0800

    added CharandBoolean.java

---
+++ CharandBoolean.java
@@ -0,0 +1,15 @@
+package com.Amee;
+
+public class CharandBoolean {
+
+    public static void main(String[] args) {
+	// width of 16 (2 bytes)
+        char mychar = '\u00A9';
+        System.out.println("Unicode output was : " + mychar);
+
+        boolean myBoolean = true;
+
+        char registeredsymbol = '\u00AE';
+        System.out.println("unicode output was : " + registeredsymbol);
+    }
+}
