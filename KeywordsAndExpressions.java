26c5e988ed62025d60d15ac7a10755d184470d40
Ravi Shah <amisankhesara@gmail.com>
2019-02-20 23:53:03 -0800

    added KeywordsAndExpressions

---
+++ KeywordsAndExpressions.java
@@ -0,0 +1,13 @@
+package com.Amee;
+
+public class KeywordsAndExpressions {
+
+    public static void main(String[] args) {
+	// write your code here
+        int score = 100;
+        if (score > 99){
+            System.out.println("You got the high score!!");
+            score = 0;
+        }
+    }
+}
