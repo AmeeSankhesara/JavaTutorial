16065904425c47c0f5b278d4a8bfe3d8d70c58d8
Ravi Shah <amisankhesara@gmail.com>
2019-02-20 23:54:14 -0800

    added StatementWhiteSpaceAndIndenting.java

---
+++ StatementWhiteSpaceAndIndenting.java
@@ -0,0 +1,16 @@
+package com.Amee;
+
+public class StatementWhiteSpaceAndIndenting {
+
+    public static void main(String[] args) {
+
+	    int myvariable = 50;
+	    myvariable++;
+	    myvariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is"+
+                "another" +
+                "Still more.");
+    }
+}
